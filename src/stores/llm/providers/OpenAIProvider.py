79dfc9f5262a64a78bdb325a047f1d9131c7d771
Ameni mhadhbi <91744121+ameni21@users.noreply.github.com>
2024-08-19 12:39:01 +0100

    nlp vector embedding

--- src/stores/llm/providers/OpenAIProvider.py
+++ src/stores/llm/providers/OpenAIProvider.py
@@ -5,7 +5,7 @@ import logging
 
 class OpenAIProvider(LLMInterface):
 
-    def __init__(self, api_key: str, api_url: str=None,
+    def __init__(self, api_key: str, api_url:str=None,
                        default_input_max_characters: int=1000,
                        default_generation_max_output_tokens: int=1000,
                        default_generation_temperature: float=0.1):
@@ -24,7 +24,7 @@ class OpenAIProvider(LLMInterface):
 
         self.client = OpenAI(
             api_key = self.api_key,
-            api_url = self.api_url
+            #api_url = self.api_url
         )
 
         self.logger = logging.getLogger(__name__)
